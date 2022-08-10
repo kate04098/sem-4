68a086a08bb3ebc5c927cd7e6ccb95137fe41536
Ekaterina <artyuhovae@mail.ru>
2022-08-10 12:31:55 +0300

    add z3

---
+++ z3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+String? inputLine = Console.ReadLine();
+int inputNum = int.Parse(inputLine);
+Console.WriteLine(sumNums(inputNum));
+int sumNums(int num)
+{ int sum = 1;
+for (int i = 1; i <= inputNum; i++)
+{ 
+//sum *=1;
+sum=sum*1;
+}
+return sum;
+}
+
