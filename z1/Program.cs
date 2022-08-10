e01479223db6d3ba192ffba6034ee45bc9ee7c8a
Ekaterina <artyuhovae@mail.ru>
2022-08-10 12:11:49 +0300

    add z1

--- z1/Program.cs
+++ z1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int inputNumber = int.Parse(Console.ReadLine());
+int t;
+void VariantSimple()
+{ 
+long sumOfNumbers = 0;
+for (long i = 1; i <= inputNumber; i++)
+{
+ sumOfNumbers += i;
+// sumOfNumbers= sumOfNumbers +i;
+}
+Console.WriteLine("Сумма чисел от 1 до" + inputNumber 
+                                    +  " = " + sumOfNumbers);
+}
+void VariantGauss()
+{
+ int sumOfNumbers = 0;
+sumOfNumbers = ((1+ inputNumber) * inputNumber) / 2;
+Console.WriteLine("Сумма чисел от 1 до" + inputNumber 
+                                    + " = " + sumOfNumbers);
+}
+t = Environment. TickCount;
+VariantSimple();
+Console.WriteLine("Simple time: {0} ms", Environment.TickCount - t);
+t = Environment. TickCount;
+VariantGauss();
+Console.WriteLine("Gauss time: {0} ms", Environment.TickCount - t);
+
