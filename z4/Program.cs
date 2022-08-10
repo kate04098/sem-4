d7df4c5442db9dd2c114538dd9c391615d748de0
Ekaterina <artyuhovae@mail.ru>
2022-08-10 12:18:49 +0300

    add z4 Please enter the commit message for your changes. Lines starting

---
+++ z4/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Random numberSintezator = new Random();
+void VariantNaive()
+{
+int i = 0;
+Console.Write("[");
+while(i<8)
+{
+Console.WriteLine(numberSintezator.Next(0,2));
+i++;
+}
+Console.Write("]");
+}
