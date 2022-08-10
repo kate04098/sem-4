2827c88be65a41f39d1a04a2316fec2089702b0c
Ekaterina <artyuhovae@mail.ru>
2022-08-10 12:29:10 +0300

    add z2

---
+++ z2/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string inputLineNumber = Console.ReadLine()??"";
+int inputNumber = int.Parse(inputLineNumber);
+int VariantChar()
+{
+//int numberLength = 0;
+//char[] array = inputLineNumber.ToCharArray();
+//numberLength = array.Length;
+return inputLineNumber.ToCharArray();
+}
+      int VariantSimple()
+{
+ int numberLength = 0;
+ int digits =1;
+while(digits<inputNumber)
+{
+digits= digits*10;
+numberLength++;
+}
+return numberLength;
+}
+int VariantLog10()
+{
+return (int) Math.Log10(inputNumber);
+}
+int result = 0;
+timePoint = DateTime.Now;
+result = VariantChar();
+  Console.WriteLine(result);
+  Console.WriteLine(DateTime.Now- timePoint);
+result = VariantSimple ();
+  Console.WriteLine(result);
+  Console.WriteLine(DateTime.Now- timePoint);
+result = VariantLog10();
+  Console.WriteLine(result);
+  Console.WriteLine(DateTime.Now- timePoint);
+
